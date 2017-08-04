10021e3003ededa57c0bb245e762885b1992daac
Ke Wang US1072104 <wangke@wangke-m01.vmware.com>
2017-08-03 22:15:08 -0700

    client benchmarking

--- src/java/main/org/apache/zookeeper/Benchmark.java
+++ src/java/main/org/apache/zookeeper/Benchmark.java
@@ -16,7 +16,7 @@ public class Benchmark {
         }
         BlockingQueue queue = new ArrayBlockingQueue(Integer.valueOf(args[3]));
         for(int i=0; i<Integer.valueOf(args[3]); i++){
-            ZKDemo zkdemo = new ZKDemo(Integer.valueOf(args[2]), "/"+i+args[4], args[1], args[0], queue);
+            ZKDemo zkdemo = new ZKDemo(Integer.valueOf(args[2]), "/"+i+args[4], args[1], args[0], queue, Integer.valueOf(args[3]));
             zkdemo.start();
         }
 

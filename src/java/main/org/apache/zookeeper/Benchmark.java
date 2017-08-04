61a4cb977633816f092494e95785196c194273bf
Ke Wang US1072104 <wangke@wangke-m01.vmware.com>
2017-08-03 21:39:46 -0700

    client benchmarking

--- src/java/main/org/apache/zookeeper/Benchmark.java
+++ src/java/main/org/apache/zookeeper/Benchmark.java
@@ -11,7 +11,7 @@ public class Benchmark {
             System.exit(0);
         }
         for(int i=0; i<Integer.valueOf(args[3]); i++){
-            ZKDemo zkdemo = new ZKDemo(Integer.valueOf(args[2]), i+args[4], args[1], args[0]);
+            ZKDemo zkdemo = new ZKDemo(Integer.valueOf(args[2]), '/'+i+args[4], args[1], args[0]);
             zkdemo.start();
         }
     }
